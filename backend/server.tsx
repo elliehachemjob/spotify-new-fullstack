9a196720da8dc3b163a83991265edacaa5942d24
elliehachemjob <elliehachemjob@gmail.com>
2021-11-29 10:39:25 +0200

    --

---
+++ backend/server.tsx
@@ -0,0 +1,28 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+
+//capital here to sepcfic this is a class we are creating
+
+app.post("Login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http/:localhost:3000/",
+    clientId: "d85bfe356da5407ba7eab2e2c4504cf9",
+    clientSecret: "41ed76e0692343239391400a52acb496",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
